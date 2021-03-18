986d4bda6c5d68c2ebb8b07a1355d6458c36d7d1
danieldroder <ddroder@gmail.com>
2021-03-18 08:13:12 -0400

    working flashing

--- app.py
+++ app.py
@@ -3,19 +3,24 @@
 #TODO: add bootstrap buttons to make things pretty
 #TODO: add flashing
 #TODO: Add user input range of pages for context (page 30 - page 150)
-#TODO:
+#TODO: General text generation
 
-from flask import Flask,url_for,request,render_template
+from flask import Flask,url_for,request,render_template,flash
 from pipelineSummarize import aiReadingModels
 from flask_bootstrap import Bootstrap
 app = Flask(__name__)
-
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/summaryModel")
+@app.route("/summaryModel",methods=['POST','GET'])
 def summary_model():
+    if request.method=="POST":
+        text=request.form['text']
+        # print(f"{text}")
+        flash(text,category="error")
+        return render_template("summaryModel.html")
     return render_template("summaryModel.html")
 
 @app.route("/qaModel")
